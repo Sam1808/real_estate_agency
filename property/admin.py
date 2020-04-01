aee9a8216c4ad040cec405f60cb06e491924e17c
Anton Pozdnyakov <anton.pozdnyakov.i@gmail.com>
2020-04-01 23:26:25 +0300

    Normalize Flat.phonenumbers into Flat.owner_phone_pure

--- property/admin.py
+++ property/admin.py
@@ -6,7 +6,15 @@ from .models import Flat, Complaint
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner','town','address')
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_display = (
+        'address',
+        'price',
+        'new_building',
+        'construction_year',
+        'town',
+        'owners_phonenumber',
+        'normalised_owners_phonenumber'
+    )
     list_editable = ['new_building']
     list_filter = ['new_building']
     raw_id_fields = ('liked_by',)
--- property/models.py
+++ property/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
+    normalised_owners_phonenumber = PhoneNumberField(verbose_name='Нормализованный номер владельца:', blank=True)
+
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
     
     description = models.TextField("Текст объявления", blank=True)
@@ -24,7 +27,8 @@ class Flat(models.Model):
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
 
     new_building = models.NullBooleanField()
-    liked_by = models.ManyToManyField(User, related_name="liked_flats", verbose_name='Кто лайкнул:')
+    liked_by = models.ManyToManyField(User, related_name="liked_flats", verbose_name='Кто лайкнул:', blank=True)
+
 
 
     def __str__(self):
